a3675cdfa70a38520c8697b5addab7a868c860e4
Alan Zhang <alan.zhang0212@gmail.com>
2023-07-20 17:40:25 -0700

    Added profile editing and best books/authors page

--- index.js
+++ index.js
@@ -7,15 +7,16 @@ const connection = require('./database');
 
 app.set('view engine', 'ejs');
 app.set('views', __dirname + '/views');
+app.use(bodyParser.urlencoded({ extended: true }));
 
 
 app.get('/', (req, res) => {
     res.render('home');
 });
 
-
 app.get('/profile', (req, res) => {
     const userEmail = 'aakasterld@illinois.edu';
+    const editing = req.query.edit === 'true';
 
     connection.query('SELECT illiniEmail, firstName, lastName, gender, introduction FROM User WHERE illiniEmail = ?', [userEmail], (error, results) => {
         if (error) {
@@ -25,29 +26,57 @@ app.get('/profile', (req, res) => {
         }
 
         const user = results[0]; // Assuming you expect a single user as the result
-        res.render('profile', { user });
+        res.render('profile', { user, editing });
     });
 });
 
-app.get('/awards', (req, res) => {
+app.post('/profile', (req, res) => {
+    const userEmail = 'aakasterld@illinois.edu';
+    const { firstName, lastName, gender, introduction } = req.body;
+
+    // Perform the SQL query to update the user information in the database
     connection.query(
-        "SELECT * FROM `Award`",
-        (error, results, fields) => {
-            if (error) throw error;
-            res.json(results);
+        'UPDATE User SET firstName=?, lastName=?, gender=?, introduction=? WHERE illiniEmail=?',
+        [firstName, lastName, gender, introduction, userEmail],
+        (error, results) => {
+            if (error) {
+                console.error('Error updating user information:', error);
+                // Handle the error appropriately, e.g., show an error page or redirect
+                return;
+            }
+            // Redirect the user back to the profile page after the update
+            res.redirect('/profile');
         }
     );
 });
 
-app.get('/users', (req, res) => {
-    connection.query(
-        "SELECT * FROM `User`",
-        (error, results, fields) => {
-            if (error) throw error;
-            res.json(results);
+
+app.get('/best', (req, res) => {
+    const topBooksQuery = 'SELECT title, rating, COUNT(awardName) as numAwards FROM Book JOIN Statistics USING(bookId) JOIN won USING(bookId) GROUP BY bookId, rating ORDER BY rating DESC, numAwards DESC LIMIT 15';
+    const topAuthorsQuery = 'SELECT authorName, COUNT(awardName) AS numAwards FROM Book JOIN writtenBy USING(bookId) JOIN won USING(bookId) GROUP BY authorName ORDER BY COUNT(awardName) DESC LIMIT 15';
+
+    connection.query(topBooksQuery, (error1, results1) => {
+        if (error1) {
+            console.error('Error executing query 1:', error1);
+            // Handle the error appropriately, e.g., show an error page or redirect
+            return;
         }
-    );
+
+        connection.query(topAuthorsQuery, (error2, results2) => {
+            if (error2) {
+                console.error('Error executing query 2:', error2);
+                // Handle the error appropriately, e.g., show an error page or redirect
+                return;
+            }
+
+            const topBooks = results1;
+            const topAuthors = results2;
+
+            res.render('best', { topBooks, topAuthors });
+        });
+    });
 });
+
 
 // Use port 8080 by default, unless configured differently in Google Cloud
 const port = process.env.PORT || 8080;
